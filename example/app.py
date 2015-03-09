ca7a55f19710aa8d167f655ac5ba83c1e075c2f0
George Theofilis <theofilis.g@gmail.com>
2015-03-09 22:35:03 +0200

    Python Example

--- example/app.py
+++ example/app.py
@@ -1,22 +1,53 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from elasticsearch import Elasticsearch
+
+from random import randint, random
 
 es = Elasticsearch()
 
-hive_id = "1"
-time_at_current_location = datetime.now()
-log_id = time_at_current_location.strftime("%Y-%m-%dT%H:%M:%SZ") + "_" + hive_id
-
-log = {
-	"hive_id": hive_id, 
-	"time_at_current_location": time_at_current_location,
-	"weight" : 3,
-    "temperature" : 32,
-    "moisture" : 75,
-    "location" :  {
-        "lat" : 38.0317237,
-        "lon" : 23.6738527
+hives = {
+    "1": {
+        "lat": 38.0317237,
+        "lon": 23.6738527
+    },
+    "2": {
+        "lat": 40.152872,
+        "lon": 22.428687
+    },
+    "3": {
+        "lat": 39.837847,
+        "lon": 22.118747
+    },
+    "4": {
+        "lat": 35.339347,
+        "lon": 24.487948
     }
+
 }
 
-print es.index(index="hivemon", doc_type="hive_logs", id=log_id, body=log)
+weight = {
+    "1": 0,
+    "2": 0,
+    "3": 0,
+    "4": 0,
+}
+
+time_at_current_location = datetime.now() - timedelta(days=7)
+
+for i in range(2000):
+    time_at_current_location = time_at_current_location + timedelta(minutes=5)
+
+    hive_id = "4"
+    log_id = time_at_current_location.strftime("%Y-%m-%dT%H:%M:%SZ") + "_" + hive_id
+    weight[hive_id] += random() / randint(1, 4)
+
+    log = {
+        "hive_id": hive_id,
+        "time_at_current_location": time_at_current_location,
+        "weight": weight[hive_id],
+        "temperature": 31,
+        "moisture": 75,
+        "location": hives[hive_id]
+    }
+
+    print es.index(index="hivemon", doc_type="hive_logs", id=log_id, body=log)
